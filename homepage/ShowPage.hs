f00e186a1f43dce60266b7ea0f69eb728efa7052
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-12-06 02:40:31 +0900

    ShowPage.hsのリファクタリング等

--- homepage/ShowPage.hs
+++ homepage/ShowPage.hs
@@ -37,16 +37,14 @@ showPage ma hdl = do
 	(cnt, mt) <- liftIO $ (,)
 		<$> (if isBinary tp then readBinaryFile else readFile) fp
 		<*> getModificationTime fp
-	putResponse hdl
-		(responseH hdl $ LBS.fromChunks [createContents fp_ mt tp cnt])
-			{ responseContentType = tp }
+	putResponse hdl (responseH hdl $ LBS.fromChunks [makePage fp_ mt tp cnt])
+		{ responseContentType = tp }
 	where
 	responseH :: HandleLike h => h -> LBS.ByteString -> Response Pipe h
 	responseH = const response
 
-createContents ::
-	FilePath -> UTCTime -> ContentType -> String -> BSC.ByteString
-createContents fp_ mt tp cnt = case (isHtml tp, isBinary tp, isGoogleCheck fp_) of
+makePage :: FilePath -> UTCTime -> ContentType -> String -> BSC.ByteString
+makePage fp_ mt tp cnt = case (isHtml tp, isBinary tp, isGoogleCheck fp_) of
 	(True, _, False) -> BSU.fromString . makeHtml fp_ mt $ span (/= '\n') cnt
 	(_, False, False) -> BSU.fromString cnt
 	_ -> BSC.pack cnt
@@ -54,7 +52,7 @@ createContents fp_ mt tp cnt = case (isHtml tp, isBinary tp, isGoogleCheck fp_) 
 
 makeHtml :: FilePath -> UTCTime -> (String, String) -> String
 makeHtml fp mt (ttl, bdy) = "<!DOCTYPE html><html lang=\"ja\">" ++ makeHead ttl
-	++ "<body>" ++ breadcrumb (splitPath fp) ++ makeMeta mt
+	++ "<body>" ++ breadcrumb (splitPath fp) ++ makeMeta mt "重城良国"
 	++ "<h1>" ++ ttl ++ "</h1>"
 	++ removeNewline False bdy ++ makeValidIcons fp ++ "</body></html>"
 
@@ -66,23 +64,20 @@ makeHead ttl = "<head><meta charset=\"UTF-8\"><title>" ++ ttl ++ "</title>"
 breadcrumb :: [String] -> String
 breadcrumb ["/"] = "<small>top</small>"
 breadcrumb ("/" : ps) = "<small><a href=\"/\">top</a> &gt; "
-	++ concat (map pathToHref $ init pairs) ++ last (map fst pairs)
-	++ "</small>"
+	++ concat (map link $ init pairs) ++ last (map fst pairs) ++ "</small>"
 	where
-	pathToHref (n, p) = "<a href=\"" ++ p ++ "\">" ++ n ++ "</a> &gt; "
-	pairs = zip
-		<$> map dropTrailingPathSeparator
-		<*> (tail . scanl (++) "/") $ ps
+	link (n, p) = "<a href=\"" ++ p ++ "\">" ++ n ++ "</a> &gt; "
+	pairs = ($ ps) $ zip
+		<$> map dropTrailingPathSeparator <*> (tail . scanl (++) "/")
 breadcrumb _ = "<small>bad path</small>"
 
-makeMeta :: UTCTime -> String
-makeMeta mt = "<div class=\"right\"><small>更新日: <time>"
-	++ formatTime defaultTimeLocale "%Y-%m-%dT%H:%M+09:00" (utcToZonedTime japan mt)
+makeMeta :: UTCTime -> String -> String
+makeMeta mt at = "<div class=\"right\"><small>更新日: <time>"
+	++ formatTime defaultTimeLocale
+		"%Y-%m-%dT%H:%M+09:00" (utcToZonedTime japan mt)
 	++ "</time></small></div>"
-	++ "<div class=\"right\"><small>文責: 重城良国</small></div>"
-	where
-	japan :: TimeZone
-	japan = TimeZone 540 False "JST"
+	++ "<div class=\"right\"><small>文責: " ++ at ++ "</small></div>"
+	where japan = TimeZone 540 False "JST"
 
 removeNewline :: Bool -> String -> String
 removeNewline _ "" = ""
@@ -91,22 +86,24 @@ removeNewline j ('\n' : c : cs)
 removeNewline _ (c : cs) = c : removeNewline (not $ isAscii c) cs
 
 makeValidIcons :: FilePath -> String
-makeValidIcons fp = "<p class=\"right\"><a href=\"" ++ pathToCssCheckerUri fp
-	++ "\"><img src=\"http://jigsaw.w3.org/css-validator/images/vcss\" width=\"44\" height=\"15\" alt=\"正当なCSSです!\"/></a>"
-	++ " <a href=\"" ++ pathToCheckerUri fp
-	++ "\"><img src=\"http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png\" width=\"41\" height=\"16\" alt=\"HTML5 Powered with CSS3 / styling, and Semantics\"/></a></p>"
+makeValidIcons fp = "<p class=\"right\"><a href=\"" ++ pathToCssCk fp ++ "\">"
+	++ "<img src=\"http://jigsaw.w3.org/css-validator/images/vcss\" "
+	++ "width=\"44\" height=\"15\" alt=\"正当なCSSです!\"/></a>"
+	++ " <a href=\"" ++ pathToCk fp ++ "\">"
+	++ "<img src=\"http://www.w3.org/html/logo/badge/"
+	++ "html5-badge-h-css3-semantics.png\" "
+	++ "width=\"41\" height=\"16\" "
+	++ "alt=\"HTML5 Powered with CSS3 / styling, and Semantics\"/></a></p>"
 
-pathToCssCheckerUri :: FilePath -> String
-pathToCssCheckerUri =
-	("http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fskami.iocikun.jp" ++)
-		. escapeSlash
+pathToCssCk :: FilePath -> String
+pathToCssCk = ("http://jigsaw.w3.org/css-validator/validator" ++) . pathToGetUri
 
-pathToCheckerUri :: FilePath -> String
-pathToCheckerUri =
-	("http://validator.w3.org/check?uri=http%3A%2F%2Fskami.iocikun.jp" ++)
-		. escapeSlash
+pathToCk :: FilePath -> String
+pathToCk = ("http://validator.w3.org/check" ++) . pathToGetUri
 
-escapeSlash :: FilePath -> String
-escapeSlash "" = ""
-escapeSlash ('/' : cs) = "%2F" ++ escapeSlash cs
-escapeSlash (c : cs) = c : escapeSlash cs
+pathToGetUri :: FilePath -> String
+pathToGetUri = ("?uri=http%3A%2F%2Fskami.iocikun.jp" ++) . escapeSlash
+	where
+	escapeSlash "" = ""
+	escapeSlash ('/' : cs) = "%2F" ++ escapeSlash cs
+	escapeSlash (c : cs) = c : escapeSlash cs
