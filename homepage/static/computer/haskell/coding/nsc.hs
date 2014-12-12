c9736fd4bd69a92828edf0b1762e5d412e34cb38
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-12-12 12:25:38 +0900

    「そんなに本気じゃない暗号化ソフト」開発を続き: decrypt1の作成を開始した

--- homepage/static/computer/haskell/coding/nsc.hs
+++ homepage/static/computer/haskell/coding/nsc.hs
@@ -4,6 +4,7 @@ import Control.Applicative
 import Control.Arrow
 import "monads-tf" Control.Monad.State
 import "monads-tf" Control.Monad.Identity
+import Data.List
 import System.Random
 import Crypto.Cipher.AES
 import Crypto.Cipher.Types
@@ -16,8 +17,8 @@ import qualified Crypto.Hash.SHA256 as SHA256
 key :: AES
 key = initAES ("passwordpassword" :: BS.ByteString)
 
-iv :: BS.ByteString
-iv = "this is iv ivivi"
+-- iv :: BS.ByteString
+-- iv = "this is iv ivivi"
 
 randomIv :: RandomGen g => AES -> g -> (BS.ByteString, g)
 randomIv a g = BS.pack `first` times (blockSize a) random g
@@ -42,15 +43,42 @@ blkSzToLen b s = (BS.length s `div` b + 1) * b
 padding :: Int -> BS.ByteString -> BS.ByteString
 padding b = ($) <$> padToLen <*> blkSzToLen b
 
+unpadding :: BS.ByteString -> BS.ByteString
+unpadding p = BS.take (BS.length p - fromIntegral (BS.last p) - 1) p
+
 encrypt1 :: RandomGen g => AES -> BS.ByteString -> g -> (BS.ByteString, g)
 encrypt1 _ "" g = ("[nsc][0]", g)
 encrypt1 k s g = let
 	(iv, g') = randomIv k g
-	cph = Base64.encode $ encryptCBC k iv (padding (blockSize k) s) in
+	cph = Base64.encode . (iv `BS.append`)
+		$ encryptCBC k iv (padding (blockSize k) s) in
 	("[nsc][" `BS.append` BSC.pack (show $ BS.length cph)
 		`BS.append` "]" `BS.append` cph, g')
 
 encrypt :: RandomGen g => AES -> BS.ByteString -> g -> (BS.ByteString, g)
-encrypt k s = runState $ BSC.unlines <$> mapM
+encrypt k str = runState $ BSC.unlines <$> mapM
 	(\s -> StateT $ Identity . encrypt1 k s)
-	(BSC.lines s)
+	(BSC.lines str)
+
+cipheredExample :: BS.ByteString
+cipheredExample = "This is ciphered example\n"
+	`BS.append` fst (encrypt (mkAes "password") "hello\nworld" (mkStdGen 12))
+
+breakNsc :: BS.ByteString -> Maybe (BS.ByteString, BS.ByteString)
+breakNsc s
+	| Just (n, t) <- nt = Just $ BS.splitAt n t
+	| otherwise = Nothing
+	where
+	nt = case BS.breakSubstring "[nsc][" s of
+		(_, "") -> Nothing
+		(_, d) -> Just $ read . BSC.unpack *** BS.tail $
+			BSC.span (/= ']') $ BS.drop 6 d
+
+decryptPP :: BS.ByteString -> [BS.ByteString]
+decryptPP = unfoldr breakNsc
+
+decrypt1 :: AES -> BS.ByteString -> Maybe BS.ByteString
+decrypt1 k l = case Base64.decode l of
+	Right d -> let (iv, cpd) = BS.splitAt 16 d in
+		Just . unpadding $ decryptCBC k iv cpd
+	_ -> Nothing
