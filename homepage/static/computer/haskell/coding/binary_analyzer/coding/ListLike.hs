10b9d71280e3d136975118621bff31f83aa40b34
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2014-12-30 16:28:01 +0900

    バイナリ解析器の作成中

---
+++ homepage/static/computer/haskell/coding/binary_analyzer/coding/ListLike.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module ListLike (ListLike(..), span) where
+
+import Prelude hiding (splitAt, span)
+import qualified Prelude as P
+
+import Data.Word8 (Word8)
+
+import qualified Data.ByteString as BS
+
+class ListLike a where
+	type Element a
+	empty :: a
+	cons :: Element a -> a -> a
+	uncons :: a -> Maybe (Element a, a)
+	splitAt :: Integer -> a -> (a, a)
+	splitAt n xs
+		| n <= 0 = (empty, xs)
+		| otherwise = case uncons xs of
+			Just (h, t) -> let
+				(t1, t2) = splitAt (n - 1) t in
+				(cons h t1, t2)
+			_ -> (empty, empty)
+
+instance ListLike [a] where
+	type Element [a] = a
+	empty = []
+	cons = (:)
+	uncons (x : xs) = Just (x, xs)
+	uncons _ = Nothing
+
+instance ListLike BS.ByteString where
+	type Element BS.ByteString = Word8
+	empty = BS.empty
+	cons = BS.cons
+	uncons = BS.uncons
+	splitAt = BS.splitAt . fromIntegral
+
+span :: ListLike a => (Element a -> Bool) -> a -> (a, a)
+span p s = case uncons s of
+	Just (h, t) | p h -> let (t1, t2) = span p t in (cons h t1, t2)
+	_ -> (empty, s)
