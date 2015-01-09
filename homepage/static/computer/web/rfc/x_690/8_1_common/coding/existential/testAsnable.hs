b7111fd30085f3c549e7815788c138167030fe68
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-01-09 21:16:10 +0900

    ASN.1の存在型を利用したデコード関数の説明に追加

--- homepage/static/computer/web/rfc/x_690/8_1_common/coding/existential/testAsnable.hs
+++ homepage/static/computer/web/rfc/x_690/8_1_common/coding/existential/testAsnable.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Applicative
 import System.IO.Unsafe
 
@@ -8,14 +10,17 @@ import Asnable
 cert :: BS.ByteString
 cert = unsafePerformIO $ BS.readFile "test_ASN_1_cert.der"
 
-data Rawest = RC Asn1Tag [Rawest] | RP Asn1Tag BS.ByteString
+ind_test :: BS.ByteString
+ind_test = "\x30\x80\x01\x01\x88\x00\x00"
+
+data Rw = RC Asn1Tag [Rw] | RP Asn1Tag BS.ByteString
 	deriving Show
 
-toRawest :: AsnableBox -> Rawest
-toRawest ab = case getAsn1Tag ab of
+toRw :: AsnableBox -> Rw
+toRw ab = case getAsn1Tag ab of
 	t@(Asn1Tag _ Constructed _) -> let
 		Just (RawConstructed _ as) = getAsnable ab in
-		RC t $ map toRawest as
+		RC t $ map toRw as
 	t -> let
 		Just (Raw _ bs) = getAsnable ab in
 		RP t bs
