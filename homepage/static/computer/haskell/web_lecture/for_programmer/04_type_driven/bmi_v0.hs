d886c636d07885a11e76eea25f2c3a0bf7be3536
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-02-04 16:05:11 +0900

    1から学ぶHaskell: 「型駆動開発」作成

---
+++ homepage/static/computer/haskell/web_lecture/for_programmer/04_type_driven/bmi_v0.hs
@@ -0,0 +1,5 @@
+bmi :: Double -> Double -> Double
+bmi = undefined
+
+isObese :: Double -> Double -> Bool
+isObese = undefined
---
+++ homepage/static/computer/haskell/web_lecture/for_programmer/04_type_driven/bmi_v1.hs
@@ -0,0 +1,5 @@
+bmi :: Double -> Double -> Double
+bmi = undefined
+
+isObese :: Double -> Double -> Bool
+isObese h w = bmi h w >= 25
