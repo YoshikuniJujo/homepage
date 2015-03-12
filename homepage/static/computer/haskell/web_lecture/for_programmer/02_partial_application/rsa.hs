050144db1e43bda8983d745622123449ff5c1e5a
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-03-12 10:47:00 +0900

    1から学ぶHaskell: 「部分適用」に課題を追加

--- homepage/static/computer/haskell/web_lecture/for_programmer/02_partial_application/rsa.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/02_partial_application/rsa.hs
@@ -5,3 +5,8 @@ decrypt1 c = xxcrypt 138689 95497 c
 
 encrypt1' = xxcrypt 138689 13
 decrypt1' = xxcrypt 138689 95497
+
+xxcrypt1 = xxcrypt 138689
+
+encrypt1'' = xxcrypt1 13
+decrypt1'' = xxcrypt1 95497
