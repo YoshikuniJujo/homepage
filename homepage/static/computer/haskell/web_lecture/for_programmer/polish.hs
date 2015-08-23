acc4a81c7209acb1e025a51b5897bdb7631b5156
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-08-23 12:42:03 +0900

    「1から学ぶHaskell」: 「ポーランド記法」の修正

--- homepage/static/computer/haskell/web_lecture/for_programmer/polish.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/polish.hs
@@ -1,16 +1,16 @@
-operators :: [(String, Integer -> Integer -> Integer)]
-operators = [("+", (+)), ("-", (-)), ("*", (*)), ("/", div)]
-
-polish :: [String] -> [Integer] -> [Integer]
-polish [] ns = ns
-polish (s : ss) ns = case lookup s operators of
-	Just o -> case polish ss ns of
-		x : y : ns -> x `o` y : ns
-		_ -> error "polish: operator needs two numbers"
-	_ -> read s : polish ss ns
-
 example1, example2, example3, example4 :: [String]
 example1 = ["+", "3", "4"]
 example2 = ["*", "+", "1", "5", "+", "2", "3"]
 example3 = ["/", "*", "+", "1", "5", "+", "2", "3", "4"]
 example4 = ["/", "*", "+", "8", "5", "-", "2", "5", "4"]
+
+operators :: [(String, Integer -> Integer -> Integer)]
+operators = [("+", (+)), ("-", (-)), ("*", (*)), ("/", div)]
+
+polish :: [String] -> [Integer]
+polish [] = []
+polish (s : ss) = case lookup s operators of
+	Just o -> case polish ss of
+		x : y : ns -> x `o` y : ns
+		_ -> error "polish: an operator needs two numbers"
+	_ -> read s : polish ss
