810043b11b7fed39971175e9ae22ea20e23f5645
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-02 10:39:04 +0900

    「1から学ぶHaskell」追加: 「関数take, drop, splitAt」2/3

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/takes.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/takes.hs
@@ -1,9 +1,19 @@
 import Data.List
 
-takeRaw :: Int -> [a] -> [a]
+takeRaw, takeF, takeU :: Int -> [a] -> [a]
 takeRaw n (x : xs) | n > 0 = x : takeRaw (n - 1) xs
 takeRaw _ _ = []
+
+takeF = flip $ foldr s (const [])
+	where s x f n | n > 0 = x : f (n - 1); s _ _ _ = []
 
 takeU = curry . unfoldr $ \nl -> case nl of
 	(n, x : xs) | n > 0 -> Just (x, (n - 1, xs))
 	_ -> Nothing
+
+dropRaw, dropF :: Int -> [a] -> [a]
+dropRaw n (x : xs) | n > 0 = dropRaw (n - 1) xs
+dropRaw _ xs = xs
+
+dropF = flip $ foldr s (const [])
+	where s _ f n | n > 0 = f $ n - 1; s x f _ = x : f 0
