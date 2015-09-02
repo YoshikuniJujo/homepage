fcb64c1c4e316a148229f94798789e1c8a4536f5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-02 13:05:48 +0900

    「1から学ぶHaskell」追加: 「関数takeWhile, dropWhile, span」2/3

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/takewhiles.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/takewhiles.hs
@@ -9,3 +9,13 @@ takeWhileF p = foldr (\x -> if p x then (x :) else const []) []
 takeWhileU p = unfoldr $ \l -> case l of
 	x : xs | p x -> Just (x, xs)
 	_ -> Nothing
+
+dropWhileRaw, dropWhileF :: (a -> Bool) -> [a] -> [a]
+dropWhileRaw p (x : xs) | p x = dropWhileRaw p xs
+dropWhileRaw _ xs = xs
+
+dropWhileF p xs = foldr s (const []) xs True where
+	s x f True
+		| p x = f True
+		| otherwise = x : f False
+	s x f False = x : f False
