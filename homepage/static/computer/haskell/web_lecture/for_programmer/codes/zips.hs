c7802a26255da9ad1adf55391bbf2b1b798fad7e
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-02 15:53:53 +0900

    「1から学ぶHaskell」追加: 「関数zip, zipWith, unzip」2/3

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/zips.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/zips.hs
@@ -1,7 +1,6 @@
 import Data.List
 
 zipRaw, zipF, zipU :: [a] -> [b] -> [(a, b)]
-
 zipRaw (x : xs) (y : ys) = (x, y) : zipRaw xs ys
 zipRaw _ _ = []
 
@@ -12,4 +11,17 @@ zipF = foldr s (const [])
 
 zipU = curry . unfoldr $ \l -> case l of
 	(x : xs, y : ys) -> Just ((x, y), (xs, ys))
+	_ -> Nothing
+
+zipWithRaw :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithRaw op (x : xs) (y : ys) = x `op` y : zipWithRaw op xs ys
+zipWithRaw _ _ _ = []
+
+zipWithF op = foldr s (const [])
+	where
+	s x f (y : ys) = x `op` y : f ys
+	s _ _ _ = []
+
+zipWithU op = curry . unfoldr $ \l -> case l of
+	(x : xs, y : ys) -> Just (x `op` y, (xs, ys))
 	_ -> Nothing
