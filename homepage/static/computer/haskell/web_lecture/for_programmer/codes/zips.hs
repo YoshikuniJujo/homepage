650722dca0d8e88803f02cbc82c880a32965f7de
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-02 16:56:02 +0900

    「1から学ぶHaskell」追加: 「関数zip, zipWith, unzip」

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/zips.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/zips.hs
@@ -1,3 +1,4 @@
+import Control.Arrow
 import Data.List
 
 zipRaw, zipF, zipU :: [a] -> [b] -> [(a, b)]
@@ -13,7 +14,7 @@ zipU = curry . unfoldr $ \l -> case l of
 	(x : xs, y : ys) -> Just ((x, y), (xs, ys))
 	_ -> Nothing
 
-zipWithRaw :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithRaw, zipWithF, zipWithU :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWithRaw op (x : xs) (y : ys) = x `op` y : zipWithRaw op xs ys
 zipWithRaw _ _ _ = []
 
@@ -25,3 +26,15 @@ zipWithF op = foldr s (const [])
 zipWithU op = curry . unfoldr $ \l -> case l of
 	(x : xs, y : ys) -> Just (x `op` y, (xs, ys))
 	_ -> Nothing
+
+unzipRaw, unzipF :: [(a, b)] -> ([a], [b])
+unzipRaw ((x, y) : xys) = (x : xs, y : ys) where (xs, ys) = unzipRaw xys
+unzipRaw _ = ([], [])
+
+unzipF = foldr (\(x, y) (xs, ys) -> (x : xs, y : ys)) ([], [])
+
+unzipRaw', unzipF' :: [(a, b)] -> ([a], [b])
+unzipRaw' ((x, y) : xys) = (x :) *** (y :) $ unzipRaw' xys
+unzipRaw' _ = ([], [])
+
+unzipF' = foldr (\(x, y) -> (x :) *** (y :)) ([], [])
