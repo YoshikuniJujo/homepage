76377549c92f5429049a682dd9534ef768e401f5
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-09-04 11:19:12 +0900

    「1から学ぶHaskell」リファクタリング: 「関数」の章

--- homepage/static/computer/haskell/web_lecture/for_programmer/02_partial_application/rsa.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/02_partial_application/rsa.hs
@@ -1,12 +1,6 @@
 xxcrypt n ed mc = mc ^ ed `mod` n
 
-encrypt1 m = xxcrypt 138689 13 m
-decrypt1 c = xxcrypt 138689 95497 c
-
-encrypt1' = xxcrypt 138689 13
-decrypt1' = xxcrypt 138689 95497
-
 xxcrypt1 = xxcrypt 138689
 
-encrypt1'' = xxcrypt1 13
-decrypt1'' = xxcrypt1 95497
+encrypt = xxcrypt1 13
+decrypt = xxcrypt1 95497
