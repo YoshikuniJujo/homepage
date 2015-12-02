1e911a8808d1dd5669623581fadd964ec06742b3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-02 09:59:52 +0900

    「Haskell: Nano Scheme」追加: 「枠組み」

--- homepage/static/computer/haskell/coding/scheme/nano_scheme/framework/codes/Environment.hs
+++ homepage/static/computer/haskell/coding/scheme/nano_scheme/framework/codes/Environment.hs
@@ -1,10 +1,18 @@
-module Environment (Env, env0) where
+module Environment (Env, env0, refer) where
 
 import qualified Data.Map as M
 
-import Value (Value, Symbol)
+import Value (Value(..), Symbol, Error(..), ErrorMessage)
+
+unboundErr :: ErrorMessage
+unboundErr = "*** ERROR: unbound variable: "
 
 type Env = M.Map Symbol Value
 
 env0 :: Env
-env0 = M.fromList []
+env0 = M.fromList [
+	("exit", DoExit)
+	]
+
+refer :: Symbol -> Env -> Either Error Value
+refer s e = maybe (Left . Error $ unboundErr ++ s) Right (M.lookup s e)
