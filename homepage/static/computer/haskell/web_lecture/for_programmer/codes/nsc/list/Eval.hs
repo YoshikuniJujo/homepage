0933de01636da227a5a6e2984ffaa6c3f1f994fc
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-07 17:10:04 +0900

    「1から学ぶHaskell」修正: 「Nano Scheme」関数evaluateをモジュールEvalへ

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/list/Eval.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/list/Eval.hs
@@ -1,9 +1,15 @@
 {-# LANGUAGE TupleSections #-}
 
-module Eval (eval) where
+module Eval (evaluate) where
 
 import Environment
 import Maybe
+
+evaluate :: [Value] -> Env -> Maybe ([Value], Env)
+evaluate [] e = Just ([], e)
+evaluate (v : vs) e = case eval v e of
+	Just (v', e') -> (\(vs', e'') -> (v' : vs', e'')) `mapply` evaluate vs e'
+	_ -> Nothing
 
 eval :: Value -> Env -> Maybe (Value, Env)
 eval (Symbol s) e = (, e) `mapply` refer s e
