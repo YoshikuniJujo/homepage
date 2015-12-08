8a82d59b4cd488fc9616a47f563c70096f436a52
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2015-12-08 17:46:24 +0900

    「1から学ぶHaskell」見直し: 「Nano Scheme: 変数の評価」

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/evalsym/Environment.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/evalsym/Environment.hs
@@ -1,18 +1,15 @@
-module Environment (Env, M.fromList, refer, Value(..), showValue, Symbol) where
+module Environment (Env, M.fromList, Value(..), showValue, Symbol) where
 
 import qualified Data.Map as M
 
 type Env = M.Map Symbol Value
 
-refer :: Symbol -> Env -> Maybe Value
-refer = M.lookup
-
 data Value
-	= Symbol Symbol
+	= Sym Symbol
 	| Int Integer
 
 showValue :: Value -> String
-showValue (Symbol s) = s
+showValue (Sym s) = s
 showValue (Int i) = show i
 
 type Symbol = String
--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/evalsym/Parse.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/nsc/evalsym/Parse.hs
@@ -1,4 +1,4 @@
-module Parse (Token, parse, tokens) where
+module Parse (Token, tokens, parse) where
 
 import Data.Char
 
@@ -14,14 +14,14 @@ tokens :: String -> Maybe [Token]
 tokens (c : s)
 	| isDigit c = let (t, r) = span isDigit s in
 		(TkInt (read $ c : t) :) `mapply` tokens r
-	| isSymCh c = let (t, r) = span isSymCh s in
+	| isSym c = let (t, r) = span isSym s in
 		(TkSym (c : t) :) `mapply` tokens r
 	| isSpace c = tokens s
 tokens "" = Just []
 tokens _ = Nothing
 
-isSymCh :: Char -> Bool
-isSymCh c = any ($ c) [isAlphaNum, (`elem` "+-*/<=>?")]
+isSym :: Char -> Bool
+isSym c = any ($ c) [isAlphaNum, (`elem` "+-*/<=>?")]
 
 parse :: [Token] -> Maybe [Value]
 parse [] = Just []
@@ -30,6 +30,6 @@ parse ts = case parse1 ts of
 	_ -> Nothing
 
 parse1 :: [Token] -> Maybe (Value, [Token])
-parse1 (TkSym s : ts) = Just (Symbol s, ts)
+parse1 (TkSym s : ts) = Just (Sym s, ts)
 parse1 (TkInt i : ts) = Just (Int i, ts)
-parse1 _ = Nothing
+parse1 [] = Nothing
