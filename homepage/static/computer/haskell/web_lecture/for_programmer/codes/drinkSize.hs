e92a168840797dfc7561b30c9c7826c3789518f3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-02-18 12:06:38 +0900

    「1から学ぶHaskell」追加: 「型クラス: Ord」

--- homepage/static/computer/haskell/web_lecture/for_programmer/codes/drinkSize.hs
+++ homepage/static/computer/haskell/web_lecture/for_programmer/codes/drinkSize.hs
@@ -6,3 +6,12 @@ instance Eq Size where
 	Grande == Grande = True
 	Venti == Venti = True
 	_ == _ = False
+
+instance Ord Size where
+	Short <= _ = True
+	_ <= Short = False
+	Tall <= _ = True
+	_ <= Tall = False
+	Grande <= _ = True
+	_ <= Grande = False
+	_ <= _ = True
